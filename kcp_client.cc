89ab35883f146de4abac1f4d59a477b3850c649b
张侨 <zhangqiao@starsee.cn>
2021-09-26 13:26:16 +0800

    reformat

---
+++ kcp_client.cc
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include "ikcp.c"
+#include "utils.h"
+
+
+static int number = 0;
+
+void loop(kcpObj *send)
+{
+  unsigned int len = sizeof(struct sockaddr_in);
+  int n, ret;
+
+  while(1)
+  {
+    isleep(1);
+
+    // ikcp_update不是调用一次两次就起作用, 要loop调用
+    // ikcp_update包含ikcp_flush, 将发送队列中的数据通过下层协议进行发送
+    ikcp_update(send->kcp, iclock());
+
+    char buf[BUF_SIZE]={0};
+
+    // 处理收消息
+    n = recvfrom(send->sockfd, buf, BUF_SIZE, MSG_DONTWAIT,
+                 (struct sockaddr *) &send->addr, &len);
+
+    if(n < 0)  // 检测是否有UDP数据包: kcp头部+data
+      continue;
+
+    printf("UDP接收到数据包  大小= %d   buf =%s\n", n, buf+24);
+
+    // 预接收数据: 调用ikcp_input将裸数据交给KCP
+    // kcp接收到下层协议传进来的数据底层数据buffer转换成kcp的数据包格式
+    // 这些数据有可能是KCP控制报文，并不是我们要的数据。
+    ret = ikcp_input(send->kcp, buf, n);
+
+    if(ret < 0)//检测ikcp_input对 buf 是否提取到真正的数据
+    {
+      // printf("ikcp_input error ret = %d\n",ret);
+      continue;
+    }
+
+    while(1)
+    {
+      //kcp将接收到的kcp数据包还原成之前kcp发送的buffer数据
+      ret = ikcp_recv(send->kcp, buf, n);
+      if(ret < 0)  //检测ikcp_recv提取到的数据
+        break;
+    }
+
+    printf("数据交互  ip = %s  port = %d\n",
+           inet_ntoa(send->addr.sin_addr), ntohs(send->addr.sin_port));
+
+    if(strcmp(buf, "Conn-OK") == 0)
+    {
+      printf("Data from Server-> %s\n", buf);
+
+      ret = ikcp_send(send->kcp, send->buff, sizeof(send->buff) );
+      printf("Client reply -> 内容[%s] 字节[%d]  ret = %d\n",
+             send->buff, (int)sizeof(send->buff), ret);
+      number++;
+      printf("第[%d]次发\n", number);
+    }
+
+    //发消息
+    if(strcmp(buf,"Server:Hello!") == 0)
+    {
+      printf("Data from Server-> %s\n", buf);
+
+      ikcp_send(send->kcp, send->buff, sizeof(send->buff));
+      number++;
+      printf("第[%d]次发\n", number);
+    }
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  printf("This is kcp client\n");
+  if(argc != 3)
+  {
+    printf("Argument Error: No server ip address and port\n");
+    return -1;
+  }
+
+  kcpObj send;
+  send.is_server = false;
+  send.ipstr = (unsigned char *)argv[1];
+  send.port  = atoi(argv[2]);
+
+  bzero(send.buff, sizeof(send.buff));
+  char msg[] = "Client: Hello!";
+  memcpy(send.buff, msg, sizeof(msg));
+
+  // 创建kcp对象把send传给kcp的user变量, TODO: user变量更新
+  ikcpcb *kcp = ikcp_create(0x1, (void *)&send);
+  kcp->output = udp_output;              // 设置kcp对象的回调函数
+  ikcp_nodelay(kcp,0, 10, 0, 0);        // 1, 10, 2, 1
+  ikcp_wndsize(kcp, 128, 128);
+  send.kcp = kcp;
+
+  init(&send);          // init client socket
+
+  char init_msg[] = "Conn";                //与服务器初次通信
+  int  ret = ikcp_send(send.kcp, init_msg, (int)sizeof(init_msg));
+  printf("ikcp_send: [%s] len=%d ret = %d\n",
+         init_msg, (int)sizeof(init_msg), ret);   //发送成功的
+
+  loop(&send);
+
+  return 0;
+}
---
+++ utils.h
@@ -0,0 +1,152 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "ikcp.h"
+
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+#include <windows.h>
+#elif !defined(__unix)
+#define __unix
+#endif
+
+#ifdef __unix
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#endif
+
+/* get system time */
+static inline void itimeofday(long *sec, long *usec)
+{
+  #if defined(__unix)
+  struct timeval time;
+  gettimeofday(&time, NULL);
+  if (sec) *sec = time.tv_sec;
+  if (usec) *usec = time.tv_usec;
+  #else
+  static long mode = 0, addsec = 0;
+  BOOL retval;
+  static IINT64 freq = 1;
+  IINT64 qpc;
+  if (mode == 0) {
+    retval = QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+    freq = (freq == 0)? 1 : freq;
+    retval = QueryPerformanceCounter((LARGE_INTEGER*)&qpc);
+    addsec = (long)time(NULL);
+    addsec = addsec - (long)((qpc / freq) & 0x7fffffff);
+    mode = 1;
+  }
+  retval = QueryPerformanceCounter((LARGE_INTEGER*)&qpc);
+  retval = retval * 2;
+  if (sec) *sec = (long)(qpc / freq) + addsec;
+  if (usec) *usec = (long)((qpc % freq) * 1000000 / freq);
+  #endif
+}
+
+/* get clock in millisecond 64 */
+static inline IINT64 iclock64(void)
+{
+  long s, u;
+  IINT64 value;
+  itimeofday(&s, &u);
+  value = ((IINT64)s) * 1000 + (u / 1000);
+  return value;
+}
+
+static inline IUINT32 iclock()
+{
+  return (IUINT32)(iclock64() & 0xfffffffful);
+}
+
+/* sleep in millisecond */
+static inline void isleep(unsigned long millisecond)
+{
+  #ifdef __unix   /* usleep( time * 1000 ); */
+  struct timespec ts;
+  ts.tv_sec = (time_t)(millisecond / 1000);
+  ts.tv_nsec = (long)((millisecond % 1000) * 1000000);
+  /*nanosleep(&ts, NULL);*/
+  usleep((millisecond << 10) - (millisecond << 4) - (millisecond << 3));
+  #elif defined(_WIN32)
+  Sleep(millisecond);
+  #endif
+}
+
+#ifndef __BUF_SIZE
+#define __BUF_SIZE
+#define BUF_SIZE    128
+#endif
+
+typedef struct {
+  bool is_server;
+  unsigned char *ipstr;
+  int port;
+  ikcpcb *kcp;
+  int sockfd;
+  struct sockaddr_in addr;        // addr for sendto and recvfrom
+  char buff[BUF_SIZE];
+} kcpObj;
+
+int udp_output(const char *buf, int len, ikcpcb *kcp, void *user) {
+  kcpObj *send = (kcpObj *)user;
+  int n = sendto(send->sockfd, buf, len, 0,
+      (struct sockaddr *) &send->addr, sizeof(struct sockaddr_in));
+  if (n >= 0)
+  {
+    // printf("udp_output: %d bytes content: [%s]\n", n, buf + 24);  // 24字节的KCP头部
+    return n;
+    // return 0;
+  }
+  else
+  {
+    printf("udp_output: %d bytes send, error\n", n);
+    return -1;
+  }
+}
+
+int init(kcpObj *send)
+{
+  send->sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  if(send->sockfd < 0)
+  {
+    perror("socket error!");
+    exit(1);
+  }
+
+  bzero(&send->addr, sizeof(send->addr));
+
+  if(send->is_server)
+  {
+    struct sockaddr_in addr;
+    addr.sin_family      = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port        = htons(send->port);
+    printf("server socket: sockfd = %d  port:%d\n",
+            send->sockfd, send->port);
+
+    if(bind(send->sockfd, (struct sockaddr *)&(addr), sizeof(struct sockaddr_in)) < 0)
+    {
+      perror("bind error");
+      exit(1);
+    }
+  }
+  else
+  {
+    send->addr.sin_family      = AF_INET;
+    send->addr.sin_addr.s_addr = inet_addr((char*)send->ipstr);
+    send->addr.sin_port        = htons(send->port);
+    printf("client socket: sockfd = %d, server ip = %s  port = %d\n",
+           send->sockfd, send->ipstr, send->port);
+  }
+  return 0;
+}
+
+#endif
